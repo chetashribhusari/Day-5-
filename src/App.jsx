ae172ead2f513316a57064fefad530e21a97a99d
unknown <Admin@VIT-SYS-1860.wdc.vidyalankarlive.com>
2026-06-12 14:17:17 +0530

    upadted

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function Register() {
 
     console.log(payload)
 
-    const response = await fetch(`${import.meta.env.REACT_VITE_BACKEND_URL}/users/register`, {
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/users/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
